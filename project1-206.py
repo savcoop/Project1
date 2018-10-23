caca2ad3f91e023b6b0c17425c59d3c8fcc10faf
Savannah Cooper <savannahcooper@587212249.guest.umich.net>
2018-10-22 21:35:33 -0400

    added updated files

--- project1-206.py
+++ project1-206.py
@@ -1,41 +1,68 @@
 import os
 import filecmp
 from dateutil.relativedelta import *
-from datetime import date
+from datetime import date, datetime
+import matplotlib.pyplot as plt 
+from math import ceil
 
 
 def getData(file):
-# get a list of dictionary objects from the file
-#Input: file name
-#Ouput: return a list of dictionary objects where
-#the keys are from the first row in the data. and the values are each of the other rows
+	file_data = open(file, 'r')
+	full_list = []
+	for line in file_data.readlines()[1:]:
+		line_data = line.split(',')
+		new_dict = {'First': line_data[0], 'Last': line_data[1], 'Email': line_data[2],'Class': line_data[3], 'DOB' : line_data[4]}
+		full_list.append(new_dict)
+	return full_list
 
-	pass
 
 def mySort(data,col):
-# Sort based on key/column
-#Input: list of dictionaries and col (key) to sort on
-#Output: Return the first item in the sorted list as a string of just: firstName lastName
+	sorted_data = sorted(data, key = lambda x: x[col])
+	final_name = sorted_data[0]["First"] + " " + sorted_data[0]["Last"]
+	return final_name
 
-	pass
-
-
+	
 def classSizes(data):
-# Create a histogram
-# Input: list of dictionaries
-# Output: Return a list of tuples sorted by the number of students in that class in
-# descending order
-# [('Senior', 26), ('Junior', 25), ('Freshman', 21), ('Sophomore', 18)]
+	senior_count = 0
+	junior_count = 0
+	sophomore_count = 0
+	freshman_count = 0 
+	for x in data:
+		if x["Class"] == "Senior":
+			senior_count += 1 
+		if x["Class"] == "Junior":
+			junior_count += 1
+		if x["Class"] == "Sophomore":
+			sophomore_count += 1
+		elif x["Class"] == "Freshman":
+			freshman_count += 1 
+	class_count = {"Senior": senior_count, "Junior": junior_count, "Sophomore": sophomore_count, "Freshman": freshman_count}
+	grade = list(class_count.keys())
+	value = list(class_count.values())
+	plt.bar(grade,value, label = "Class Distribution")
+	plt.show()
 
-	pass
+	sorted_class_count = sorted(list(class_count.items()), reverse=True, key=lambda x: x[1])
 
 
+	return sorted_class_count
+
+	
 def findMonth(a):
-# Find the most common birth month form this data
-# Input: list of dictionaries
-# Output: Return the month (1-12) that had the most births in the data
+	birthday_dict = {}
+	for x in a: 
+		month = x['DOB'].split('/')[0]
+		birth_strip = month.strip()
+		if birth_strip not in birthday_dict:
+			birthday_dict[birth_strip] = 1
+		else: 
+			birthday_dict[birth_strip] += 1
 
-	pass
+	sorted_birthdays = sorted(birthday_dict.items(), key = lambda x: x[1], reverse= True)
+
+	return int(sorted_birthdays[0][0])
+
+	
 
 def mySortPrint(a,col,fileName):
 #Similar to mySort, but instead of returning single
@@ -43,8 +70,14 @@ def mySortPrint(a,col,fileName):
 # as fist,last,email
 #Input: list of dictionaries, col (key) to sort by and output file name
 #Output: No return value, but the file is written
-
-	pass
+	outFile = open(str(fileName), "w")
+	data = sorted(a, key = lambda x: x[str(col)])
+	for y in data: 
+		first = y["First"]
+		last = y["Last"]
+		email = y["Email"]
+		outFile.write(first + last + email + '\n')
+	outFile.close()
 
 def findAge(a):
 # def findAge(a):
@@ -53,8 +86,20 @@ def findAge(a):
 # integer.  You will need to work with the DOB and the current date to find the current
 # age in years.
 
-	pass
+#URL for where this solution partially came from in case they expect you to cite it:  
+#https://stackoverflow.com/questions/2217488/age-from-birthdate-in-python
+	ages = []
+	today = date.today()
+	for x in a[1:]:
+		birth_date = datetime.strptime(x['DOB'].strip(), '%m/%d/%Y')
+		age = today.year - birth_date.year - ((today.month, today.day) < (birth_date.month, birth_date.day))
+		ages.append(age)
 
+	total = 0
+	for x in ages:
+		total += x
+	avg_age = ceil(float(total/len(ages)))
+	return round(avg_age)
 
 ################################################################
 ## DO NOT MODIFY ANY CODE BELOW THIS
